12da074462cab70b6d8b15c25ba1d2ec80f79ba7
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-08-13 16:33:14 +0300

    react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+import RegPage from "./pages/RegPage/RegPage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+   <Routes>
+    <Route path="/" element = {<HomePage/>}></Route>
+    <Route path="/auth" element = {<AuthPage/>}></Route>
+    <Route path="/reg" element = {<RegPage/>}></Route>
+   </Routes>
   );
 }
 
